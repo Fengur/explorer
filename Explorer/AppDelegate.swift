b0ce04fa0c3780b92dd4b1b94e33d3124022c6fc
Eonist <unicornhive@gmail.com>
2016-03-13 10:32:02 +0100

    Files modified: 1
    Modified 1 file:Explorer/AppDelegate.swift

--- Explorer/AppDelegate.swift
+++ Explorer/AppDelegate.swift
@@ -7,8 +7,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        StyleManager.addStyle("Window Element#background{fill:#EFEFF4;corner-radius:4px;}")//<--you should target a bg element not the window it self, since now everything inherits these values
-        //win = Win(1000,800)
+        StyleManager.addStyle("Window{fill:#EFEFF4;corner-radius:4px;}Button{fill:blue;}")//<--you should target a bg element not the window it self, since now everything inherits these values
+        win = Win(1000,800)
         
         
         //Continue here:
