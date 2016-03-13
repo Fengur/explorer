e30d9e81c5ed4a96ea59544dbdc09d81a1de4ebf
Eonist <unicornhive@gmail.com>
2016-03-13 10:26:34 +0100

    Files modified: 1
    Modified 1 file:Explorer/AppDelegate.swift

--- Explorer/AppDelegate.swift
+++ Explorer/AppDelegate.swift
@@ -8,7 +8,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         StyleManager.addStyle("Window Element#background{fill:#EFEFF4;corner-radius:4px;}")//<--you should target a bg element not the window it self, since now everything inherits these values
-        win = Win(1000,800)
+        //win = Win(1000,800)
+        
+        
+        //Continue here:
+        win = FlippedView(NSRect(0,0,300,300))
+        
+        
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
     }
     func applicationWillTerminate(aNotification: NSNotification) {
