1e5178c23b63a7b3b0da479ca1520e0520dab6cf
Eonist <unicornhive@gmail.com>
2016-03-14 13:53:59 +0100

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -92,78 +92,7 @@ class BasicView:CustomView {
      * iconButtonSection
      */
     func createIconButtons(){
-        var css:String = "SelectButton{"
-        css += "line:grey7,none;"
-        css += "line-offset-type:outside;"
-        css += "line-alpha:1;"
-        css += "line-thickness:1px;"
-        css += "margin-top:0px,1px;"
-        css += "margin-left:0px,1px;"
-        css += "width:24px,24px;"
-        css += "height:24px,24px;"
-        css += "drop-shadow:drop-shadow(0px 0 #FF0000 0.0 0 0 0 0 true);"
-        css += "}"
-        
-        css += "SelectButton#first{"
-        css += "fill:linear-gradient(top,#FFFEFE,#E8E8E8),~/Desktop/svg/icons/view/icons.svg grey5;"
-        css += "corner-radius:4px 0px 4px 0px;"
-        css += "}"
-        
-        css += "SelectButton#first:selected{"
-        css += "fill:linear-gradient(top,grey9,grey8),~/Desktop/svg/icons/view/icons.svg white;"
-        css += "}"
-        
-        css += "SelectButton#second{"
-        css += "fill:linear-gradient(top,#FFFEFE,#E8E8E8),~/Desktop/svg/icons/view/list.svg grey5;"
-        css += "corner-radius:0px;"
-        css += "line-offset-type-right:inside;"
-        css += "line-offset-type-left:inside;"
-        css += "margin-left:0px,0px;"/*bug fix, sort it out later*/
-        css += "}"
-        
-        css += "SelectButton#second:over{"/*bug fix, sort it out later*/
-        css += "line-offset-type-right:inside;"
-        css += "line-offset-type-left:inside;"
-        css += "}"
-        
-        css += "SelectButton#second:selected{"
-        css += "fill:linear-gradient(top,grey9,grey8),~/Desktop/svg/icons/view/list.svg white;"
-        css += "margin-left:0px,0px;"/*bug fix, sort it out later*/
-        css += "}"
-        
-        css += "SelectButton#third{"
-        css += "fill:linear-gradient(top,#FFFEFE,#E8E8E8),~/Desktop/svg/icons/view/columns.svg grey5;"
-        css += "corner-radius:0px;"
-        css += "line-offset-type-right:inside;"
-        css += "}"
-        
-        css += "SelectButton#third:selected{"
-        css += "fill:linear-gradient(top,grey9,grey8),~/Desktop/svg/icons/view/columns.svg white;"
-        css += "}"
-        
-        css += "SelectButton#fourth{"
-        css += "fill:linear-gradient(top,#FFFEFE,#E8E8E8),~/Desktop/svg/icons/view/coverflow.svg grey5;"
-        css += "corner-radius:0px 4px 0px 4px;"
-        css += "}"
-        
-        css += "SelectButton#fourth:selected{"
-        css += "fill:linear-gradient(top,grey9,grey8),~/Desktop/svg/icons/view/coverflow.svg white;"
-        css += "}"
-        /**/
-        css += "SelectButton:selected{"
-        css += "drop-shadow:drop-shadow(0px 0 #000000 0.4 4 4 1 2 true);"
-        css += "}"
-        
-        
-        /**/
-        
-        let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
-        
-        //let styleProperty = styleCollection.getStyle("SelectButton")?.getStyleProperty("corner-radius")
-        //Swift.print("styleProperty.value: " + String(styleProperty!.value))
-        StyleManager.addStyle(styleCollection.styles)
-        
-        
+                
         let card:Card = container.addSubView(Card(NaN, NaN, "Icon button: ", container, "iconButtonCard"))
         
         
@@ -185,12 +114,6 @@ class BasicView:CustomView {
         let iconButton4 = SelectButton(24,24,false,card,"fourth")
         card.addSubView(iconButton4)
         iconButton4.setPosition(CGPoint(10+24+2+24+24+1,20))/**/
-         /**/
-        
-        
-        
-        
-        
         
         let selectGroup = SelectGroup([iconButton1,iconButton2,iconButton3,iconButton4/**/],iconButton2);
         selectGroup
