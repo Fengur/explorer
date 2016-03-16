4f7b099be955ac625e01a9fa12ebdcbbd344e43d
Eonist <unicornhive@gmail.com>
2016-03-16 06:30:29 +0100

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -156,12 +156,12 @@ class BasicView:CustomView {
      */
     func createRadioButton(){
         let radioButtonCard:Card = container.addSubView(Card(NaN, NaN, "Radio buttons: ", container, "radioButtonSection"))
-        let radioButtonContainer:Element = radioButtonSection.addSubView(new Element(NaN,NaN,radioButtonSection,"radioButtonContainer"))// :TODO: rename to background
-        let radioButton1:RadioButton = radioButtonContainer.addChild(new RadioButton(NaN,NaN,false,false,"Option 1",false,radioButtonContainer)) as RadioButton;
-        let radioButton2:RadioButton = radioButtonContainer.addChild(new RadioButton(NaN,NaN,false,false,"Option 2",true,radioButtonContainer)) as RadioButton;
-        let selectGroup:SelectGroup = radioButtonContainer.addChild(new SelectGroup([radioButton1,radioButton2],radioButton2)) as SelectGroup;
-        func onSelectGroupChange(event:SelectGroupEvent):void{
-            trace("event.selectable: " + event.selectable);
+        let radioButtonContainer:Element = radioButtonCard.addSubView(Section(NaN,NaN,radioButtonCard,"radioButtonContainer"))// :TODO: rename to background
+        let radioButton1:RadioButton = radioButtonContainer.addSubView(RadioButton(NaN,NaN,false,false,"Option 1",false,radioButtonContainer))
+        let radioButton2:RadioButton = radioButtonContainer.addSubView(RadioButton(NaN,NaN,false,false,"Option 2",true,radioButtonContainer))
+        let selectGroup:SelectGroup = radioButtonContainer.addSubView(SelectGroup([radioButton1,radioButton2],radioButton2))
+        func onSelectGroupChange(event:SelectGroupEvent){
+            Swift.print("event.selectable: " + event.selectable);
         }
         selectGroup.addEventListener(SelectGroupEvent.SELECT_GROUP_CHANGE, onSelectGroupChange);
     }
