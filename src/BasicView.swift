0b4eee3835dea855d82921d3a70af357a16435d1
Eonist <unicornhive@gmail.com>
2016-03-17 17:33:04 +0100

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -7,7 +7,7 @@ class BasicView:CustomView {
         super.resolveSkin()
         container = addSubView(Container(1000,800,self,"main"))
         
-        /*
+        /**/
         createButton()
         createTextButton()
         createRadioBullet()
@@ -30,7 +30,7 @@ class BasicView:CustomView {
         createHNodeSlider()
         createList()
         createSliderList()
-        */
+
         createComboBox()
     }
     /**
