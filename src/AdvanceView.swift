85247208884bb4c3647c4defa87afd612f5ae6a8
Eonist <unicornhive@gmail.com>
2016-03-31 06:44:10 +0200

    Files modified: 1
    Modified 1 file:src/AdvanceView.swift

--- src/AdvanceView.swift
+++ src/AdvanceView.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class AdvanceView {
-
+class AdvanceView:CustomView {
+    var container:Container!
+    override func resolveSkin() {
+        super.resolveSkin()
+        container = addSubView(Container(1000,800,self,"main"))
+    }
 }
