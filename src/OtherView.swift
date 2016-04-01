1005a092f1e55af937640d10c6b1bc2fe0658b4d
Eonist <unicornhive@gmail.com>
2016-04-01 09:13:31 +0200

    Files modified: 1
    Modified 1 file:src/OtherView.swift

--- src/OtherView.swift
+++ src/OtherView.swift
@@ -1,5 +1,19 @@
-import Foundation
+import Cocoa
 
-class OtherView {
-
+class OtherView:CustomView {
+    var container:Container!
+    override func resolveSkin() {
+        super.resolveSkin()
+        container = addSubView(Container(1000,800,self,"main"))
+        createColorBox()
+    }
+    /**
+     *
+     */
+    func createColorBox(){
+        let colorBoxCard:Card = addSubView(Card(NaN, NaN, "ColorBox: ", self, "colorBoxCard"))
+        colorBoxCard
+        let colorBox = colorBoxCard.addSubView(ColorBox(NaN,NaN,NSColor.cyanColor(),colorBoxCard))
+        colorBox
+    }
 }
