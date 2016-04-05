68b5252aa3390076296f1de8d03116d92795ba64
Eonist <unicornhive@gmail.com>
2016-04-05 18:26:04 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -7,17 +7,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        /*
+        /**/
         StyleManager.addStylesByURL("~/Desktop/css/window.css")
         StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
         win = TranslucencyWin()//Win(1000,800)
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
-        */
+
         
         //continue here: test hsb2
         //testingHSB2()
         
-        Swift.print("-60 % 360: " + "\(360 % -60)")
+        //Swift.print("-60 % 360: " + "\(360 % -60)")
         
         //testingHSB()
     }
