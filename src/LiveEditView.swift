4ca30deb1b67b577d1e7239020a5c23e65065a57
Eonist <unicornhive@gmail.com>
2016-04-08 07:22:52 +0200

    Files modified: 1
    Modified 1 file:src/LiveEditView.swift

--- src/LiveEditView.swift
+++ src/LiveEditView.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class LiveEditView {
-
+class LiveEditView:CustomView {
+    var container:Container!
+    override func resolveSkin() {
+        super.resolveSkin()
+        container = addSubView(Container(1000,800,self,"main"))
+    }
 }
