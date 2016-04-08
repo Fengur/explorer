877396488e0f85b0790edff81a25763245f5eb16
Eonist <unicornhive@gmail.com>
2016-04-08 07:31:10 +0200

    Files modified: 1
    Modified 1 file:src/LiveEditView.swift

--- src/LiveEditView.swift
+++ src/LiveEditView.swift
@@ -14,5 +14,15 @@ class LiveEditView:CustomView {
         StyleManager.addStyle("#redBox{fill:red;float:left;clear:left;}")
         let redbox = addSubView(Element(100,100,self,"redBox"))
         redbox
+        
+        StyleManager.addStyle("#btn1{fill:green;float:left;clear:left;}")
+        let btn = addSubView(Button(100,20,self,"btn1"))
+        
+        func onButtonClick(event:Event){
+            if(event.type == ButtonEvent.upInside){
+                Swift.print("click")
+            }
+        }
+        btn.event = onButtonClick
     }
 }
