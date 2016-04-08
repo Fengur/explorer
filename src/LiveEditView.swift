e6875e35a9188865e777a3bf41a48e3f9e5ac637
Eonist <unicornhive@gmail.com>
2016-04-08 13:26:36 +0200

    Files modified: 1
    Modified 1 file:src/LiveEditView.swift

--- src/LiveEditView.swift
+++ src/LiveEditView.swift
@@ -12,11 +12,16 @@ class LiveEditView:CustomView {
         Swift.print("~/Desktop/".tildePath)
         
         let theURL:NSURL = FilePathParser.path("~/Desktop/".tildePath)
+        /*
         self.directoryObserver = DirectoryObserver(URL: theURL, block: { [weak self] in
             Swift.print("change: " + "\(self)")
             
             //self?.doSomething()
             })
+        */
+        
+        FileSystemWatcher()
+        
     }
     /**
      *
@@ -100,4 +105,78 @@ class DirectoryObserver {
     
     private let fileDescriptor: CInt
     private let source: dispatch_source_t
+}
+
+public class FileSystemWatcher {
+    
+    // MARK: - Initialization / Deinitialization
+    
+    public init(pathsToWatch: [String], sinceWhen: FSEventStreamEventId) {
+        self.lastEventId = sinceWhen
+        self.pathsToWatch = pathsToWatch
+    }
+    
+    convenience public init(pathsToWatch: [String]) {
+        self.init(pathsToWatch: pathsToWatch, sinceWhen: FSEventStreamEventId(kFSEventStreamEventIdSinceNow))
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    // MARK: - Private Properties
+    
+    private let eventCallback: FSEventStreamCallback = { (stream: ConstFSEventStreamRef, contextInfo: UnsafeMutablePointer<Void>, numEvents: Int, eventPaths: UnsafeMutablePointer<Void>, eventFlags: UnsafePointer<FSEventStreamEventFlags>, eventIds: UnsafePointer<FSEventStreamEventId>) in
+        Swift.print("***** FSEventCallback Fired *****", terminator: "\n")
+        
+        let fileSystemWatcher: FileSystemWatcher = unsafeBitCast(contextInfo, FileSystemWatcher.self)
+        let paths = unsafeBitCast(eventPaths, NSArray.self) as! [String]
+        
+        for index in 0..<numEvents {
+            fileSystemWatcher.processEvent(eventIds[index], eventPath: paths[index], eventFlags: eventFlags[index])
+        }
+        
+        fileSystemWatcher.lastEventId = eventIds[numEvents - 1]
+    }
+    private let pathsToWatch: [String]
+    private var started = false
+    private var streamRef: FSEventStreamRef!
+    
+    // MARK: - Private Methods
+    
+    private func processEvent(eventId: FSEventStreamEventId, eventPath: String, eventFlags: FSEventStreamEventFlags) {
+        Swift.print("\t\(eventId) - \(eventFlags) - \(eventPath)", terminator: "\n")
+    }
+    
+    // MARK: - Pubic Properties
+    
+    public private(set) var lastEventId: FSEventStreamEventId
+    
+    // MARK: - Pubic Methods
+    
+    public func start() {
+        guard started == false else { return }
+        
+        var context = FSEventStreamContext(version: 0, info: nil, retain: nil, release: nil, copyDescription: nil)
+        context.info = UnsafeMutablePointer<Void>(unsafeAddressOf(self))
+        let flags = UInt32(kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagFileEvents)
+        streamRef = FSEventStreamCreate(kCFAllocatorDefault, eventCallback, &context, pathsToWatch, lastEventId, 0, flags)
+        
+        FSEventStreamScheduleWithRunLoop(streamRef, CFRunLoopGetMain(), kCFRunLoopDefaultMode)
+        FSEventStreamStart(streamRef)
+        
+        started = true
+    }
+    
+    public func stop() {
+        guard started == true else { return }
+        
+        FSEventStreamStop(streamRef)
+        FSEventStreamInvalidate(streamRef)
+        FSEventStreamRelease(streamRef)
+        streamRef = nil
+        
+        started = false
+    }
+    
 }
