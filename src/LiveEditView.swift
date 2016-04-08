162e8258858bae3d6f9d0e2ff09b6caba6d5cffb
Eonist <unicornhive@gmail.com>
2016-04-08 17:07:46 +0200

    Files modified: 1
    Modified 1 file:src/LiveEditView.swift

--- src/LiveEditView.swift
+++ src/LiveEditView.swift
@@ -2,7 +2,7 @@ import Cocoa
 
 class LiveEditView:CustomView {
     var container:Container!
-    var directoryObserver:DirectoryObserver?
+    
     override func resolveSkin() {
         super.resolveSkin()
         container = addSubView(Container(1000,800,self,"main"))
@@ -35,7 +35,7 @@ class LiveEditView:CustomView {
         */
         /**/
         
-        let fileWatcher = FileWatcher(pathsToWatch: ["~/Desktop/test".tildePath])
+        let fileWatcher = FileWatcher(["~/Desktop/test".tildePath])
         fileWatcher.start()
     }
     /**
@@ -100,82 +100,12 @@ class LiveEditView:CustomView {
         darkThemeButton.event = onButtonClick
     }
 }
-
-public class FolderMonitor {
-    
-    enum State {
-        case On, Off
-    }
-    
-    private let source: dispatch_source_t
-    private let descriptor: CInt
-    private let qq: dispatch_queue_t = dispatch_get_main_queue()
-    private var state: State = .Off
-    
-    /// Creates a folder monitor object with monitoring enabled.
-    public init(url: NSURL, handler: ()->Void) {
-        
-        state = .Off
-        descriptor = open(url.fileSystemRepresentation, O_EVTONLY)
-        
-        source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,UInt(descriptor),DISPATCH_VNODE_WRITE,qq)
-        
-        dispatch_source_set_event_handler(source, handler)
-        start()
-    }
-    
-    /// Starts sending notifications if currently stopped
-    public func start() {
-        if state == .Off {
-            state = .On
-            dispatch_resume(source)
-        }
-    }
-    
-    /// Stops sending notifications if currently enabled
-    public func stop() {
-        if state == .On {
-            state = .Off
-            dispatch_suspend(source)
-        }
-    }
-    
-    deinit {
-        close(descriptor)
-        dispatch_source_cancel(source)
-    }
-}
-
-class DirectoryObserver {
-    
-    deinit {
-        
-        dispatch_source_cancel(source)
-        close(fileDescriptor)
-    }
-    
-    init(URL: NSURL, block: dispatch_block_t) {
-        
-        fileDescriptor = open(URL.path!, O_EVTONLY)
-        
-        source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE,UInt(fileDescriptor),DISPATCH_VNODE_WRITE,dispatch_get_main_queue())
-        //source = dispatch_source_create(DISPATCH_SOURCE_TYPE_VNODE, UInt(fileDescriptor), DISPATCH_VNODE_WRITE, dispatch_queue_create(nil, DISPATCH_QUEUE_CONCURRENT))
-        dispatch_source_set_event_handler(source, { dispatch_async(dispatch_get_main_queue(), block) })
-        dispatch_resume(source)
-    }
-    
-    //
-    
-    private let fileDescriptor: CInt
-    private let source: dispatch_source_t
-}
-
 class FileWatcher {
     private let paths: [String]
     private var started = false
     private var streamRef: FSEventStreamRef!
     var lastEventId: FSEventStreamEventId
-    init(paths: [String], sinceWhen: FSEventStreamEventId) {
+    init(_ paths: [String], _ sinceWhen: FSEventStreamEventId) {
         self.lastEventId = sinceWhen
         self.paths = paths
     }
@@ -239,7 +169,7 @@ class FileWatcher {
     }
 }
 extension FileWatcher{
-    convenience init(pathsToWatch: [String]) {
-        self.init(pathsToWatch: pathsToWatch, sinceWhen: FSEventStreamEventId(kFSEventStreamEventIdSinceNow))
+    convenience init(_ pathsToWatch: [String]) {
+        self.init(pathsToWatch, FSEventStreamEventId(kFSEventStreamEventIdSinceNow))
     }
 }
