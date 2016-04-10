ef04500b5dba40ad89ce48f8a37736bb8b767ded
Eonist <unicornhive@gmail.com>
2016-04-10 09:21:24 +0200

    Files modified: 1
    Modified 1 file:src/LiveEditView.swift

--- src/LiveEditView.swift
+++ src/LiveEditView.swift
@@ -59,7 +59,9 @@ class LiveEditView:CustomView {
         fileWatcher!.onFileChange = { [weak self] eventId, eventPath, eventFlags in
             //let url = NSURL(fileURLWithPath: eventPath)
             Swift.print("onFileChange() " + "\(self?.temp)")
-            
+            if (eventFlags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0 {// file modified
+                Swift.print("File modified: \(eventPath) - \(eventId)")
+            }
         }
         
         fileWatcher!.start()
