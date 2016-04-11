a61d578a4ece48f56577d3ae8b693571d1bec5d6
Eonist <unicornhive@gmail.com>
2016-04-11 12:23:14 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -7,10 +7,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        /**/
         StyleManager.addStylesByURL("~/Desktop/css/window.css")
         //StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
-        win = Win(1000,800)//TranslucencyWin()//
+        win = Win(200,200/*1000,800*/)//TranslucencyWin()//
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
     }
     func applicationWillTerminate(aNotification: NSNotification) {
