8ec66527b99c141a4e5c04121aa4644abae420e2
Eonist <unicornhive@gmail.com>
2016-04-11 12:33:39 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         StyleManager.addStylesByURL("~/Desktop/css/window.css")
         StyleManager.addStylesByURL("~/Desktop/css/explorer/card.css")
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer/explorer.css")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer/explorer.css")
         //StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
         win = Win(200,200/*1000,800*/)//TranslucencyWin()//
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
