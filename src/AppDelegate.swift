3dbc1a0aab585482a165847dec713facbc415b5f
Eonist <unicornhive@gmail.com>
2016-04-11 12:10:46 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -22,48 +22,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //testingHSB()
     }
     
-    /**
-     *
-     */
-    func testingHSB(){
-        let nsColor:NSColor = NSColor.magentaColor()
-        var rgb:RGB = RGBParser.rgb(nsColor)
-        Swift.print("rgb.r: " + "\(rgb.r)")
-        Swift.print("rgb.g: " + "\(rgb.g)")
-        Swift.print("rgb.b: " + "\(rgb.b)")
-        Swift.print("")
-        
-        let hsb:HSB = HSBParser.hsb(rgb)
-        Swift.print("hsb.h: " + "\(hsb.h)")
-        Swift.print("hsb.s: " + "\(hsb.s)")
-        Swift.print("hsb.b: " + "\(hsb.b)")
-        Swift.print("")
-        
-        rgb = RGBParser.rgb(hsb)
-        Swift.print("rgb.r: " + "\(rgb.r)")
-        Swift.print("rgb.g: " + "\(rgb.g)")
-        Swift.print("rgb.b: " + "\(rgb.b)")
-    }
-    /**
-     * NOTE: tests rgb.nsCOlor and nsCOlor.hexString for the regular colors. whats wrong?
-     */
-    func colorTesting(){
-        let red:String = "0xFF0000"
-        //let green:String = "0x00FF00"
-        //let blue:String = "0x0000FF"
-        
-        let r:NSColor = NSColorParser.nsColor(red)
-        let redRGB = r.rgb
-        Swift.print("redRGB.r: " + "\(redRGB.r)")
-        Swift.print("redRGB.g: " + "\(redRGB.g)")
-        Swift.print("redRGB.b: " + "\(redRGB.b)")
-        
-        let tempColor = NSColorParser.nsColor(redRGB)
-        Swift.print("tempColor.redComponent: " + "\(tempColor.redComponent)")
-        Swift.print("tempColor.greenComponent: " + "\(tempColor.greenComponent)")
-        Swift.print("tempColor.blueComponent: " + "\(tempColor.blueComponent)")
-    }
-    
     func applicationWillTerminate(aNotification: NSNotification) {
         //print("Good-bye")
     }
