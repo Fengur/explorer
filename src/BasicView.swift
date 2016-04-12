4541989b04cd8b81b7424041486b3759c148ea1f
Eonist <unicornhive@gmail.com>
2016-04-12 14:08:41 +0200

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -74,21 +74,7 @@ class BasicView:CustomView {
     func createTextButton(){
         
         let url:String = "~/Desktop/ElCapitan/basic/button/textbutton.css"
-        StyleManager.addStylesByURL(url,true)
-        
-        fileWatcher = FileWatcher([url.tildePath])
-        
-        fileWatcher!.event = { [weak self] event in
-            Swift.print(self)
-            Swift.print(event.description)
-            if(event.fileChange && event.path == url.tildePath) {
-                StyleManager.addStylesByURL(url,true)
-                ElementModifier.refreshSkin(self!)
-                ElementModifier.floatChildren(self!)
-            }
-        }
-        fileWatcher!.start()
-        
+        StyleManager.addStylesByURL(url,false)
         
         let card:Card = container.addSubView(Card(NaN, NaN, "TextButton: ", container, "textButtonCard"))
         card.addSubView(TextButton("Button",NaN,NaN,card))
