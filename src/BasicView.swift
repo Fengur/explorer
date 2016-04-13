8bd2d4f365a3fe7f1b09a871b319ca7a4347d88d
Eonist <unicornhive@gmail.com>
2016-04-13 13:47:41 +0200

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -310,7 +310,7 @@ class BasicView:CustomView {
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml")
         let dp:DataProvider = DataProvider(xml)
         let list:List = listCard.addSubView(List(140, 72, NaN, dp,listCard))
-        ListModifier.selectAt(list, 1);
+        //ListModifier.selectAt(list, 1);
     }
     func createSliderList(){
         let sliderListCard:Card = container.addSubView(Card(NaN, NaN, "Slider list: ", container, "sliderListCard"))
