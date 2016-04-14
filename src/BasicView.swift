01924ed97b58af27da9d73935dfb3014c6bcc6d4
Eonist <unicornhive@gmail.com>
2016-04-14 15:55:14 +0200

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -31,7 +31,7 @@ class BasicView:CustomView {
         createSliderTextArea()
         createText()
         createSingleLineTextArea()
-        //createSearchBox()
+        createSearchBox()
         //createColorTag()
         //
         //createTextInput()
