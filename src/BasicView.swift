788ffbb6261a4efcf6c0141af5b9067ca76b4b08
Eonist <unicornhive@gmail.com>
2016-08-22 12:08:28 +0200

    Files modified: 1
    Modified 1 file:src/BasicView.swift

--- src/BasicView.swift
+++ src/BasicView.swift
@@ -6,6 +6,7 @@ class BasicView:CustomView {
         super.resolveSkin()
         container = addSubView(Container(1000,800,self,"main"))
         
+        /*
         createButton()
         createTextButton()
         createRadioBullet()
@@ -30,9 +31,34 @@ class BasicView:CustomView {
         createSearchBox()
         createTextInput()
         createColorTag()
-        
+        */
         //createVNodeSlider()
         //createHNodeSlider()
+        
+        /*Gradients*/
+        let gradient = LinearGradient(Gradients.red(),[],π/2)
+        let lineGradient = LinearGradient(Gradients.teal(0.5),[],π/2)
+        /*Styles*/
+        let fill:GradientFillStyle = GradientFillStyle(gradient);
+        let lineStyle = LineStyle(20,NSColorParser.nsColor(Colors.green()).alpha(0.5),CGLineCap.Round)
+        let line = GradientLineStyle(lineGradient,lineStyle)
+        /*Rect*/
+        let rect = RectGraphic(40,40,200,200,fill,line)
+        addSubview(rect.graphic)
+        rect.draw()
+        /*Ellipse*/
+        let ellipse = EllipseGraphic(300,40,200,200,fill.mix(Gradients.teal()),line.mix(Gradients.blue(0.5)))
+        addSubview(ellipse.graphic)
+        ellipse.draw()
+        /*RoundRect*/
+        let roundRect = RoundRectGraphic(40,300,200,200,Fillet(50),fill.mix(Gradients.orange()),line.mix(Gradients.yellow(0.5)))
+        addSubview(roundRect.graphic)
+        roundRect.draw()
+        /*Line*/
+        let lineGraphic = LineGraphic(CGPoint(300,300),CGPoint(500,500),line.mix(Gradients.deepPurple()))
+        addSubview(lineGraphic.graphic)
+        lineGraphic.draw()
+        
     }
     /**
      *
