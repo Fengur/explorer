e928e1dc8bd5240494211e86008afd4ac59cdc7c
Eonist <unicornhive@gmail.com>
2016-09-11 17:46:56 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -7,27 +7,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification:NSNotification) {
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/window.css")
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/card.css")
-        StyleManager.addStylesByURL("~/Desktop/ElCapitan/basic/button/button.css")
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css")
-        
+        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/basic/button/button.css")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css")
+        win = TranslucencyWin()//Win(400,300/**//*1000,800*/)//()//
+        NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
+    }
+    /**
+     * Testing
+     */
+    func styleTest(){
         //let startTime = NSDate()
-        //createComponents()
         let selectors:[ISelector] = [Selector("Window",[],""),Selector("List",[],""),Selector("Button",[],"",[])]//
         let style = StyleResolver.style2(selectors)
         style.describe()
-        /*
+
+        //Swift.print("time: " + "\(abs(startTime.timeIntervalSinceNow))")
+        Swift.print("StyleResolver.styleLookUpCount: " + "\(StyleResolver.styleLookUpCount)")
+    
         for var i = 0; i < 2000; ++i{
             
         }
-        */
-        //Swift.print("time: " + "\(abs(startTime.timeIntervalSinceNow))")
-        Swift.print("StyleResolver.styleLookUpCount: " + "\(StyleResolver.styleLookUpCount)")
-        
-        //StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
-        //win = TranslucencyWin()//Win(400,300/**//*1000,800*/)//()//
-        NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         //print("Good-bye")
