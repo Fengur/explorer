96e049270d14ebcd4401c62a702fb32a36e2ed88
Eonist <unicornhive@gmail.com>
2016-09-21 19:23:16 +0200

    Files modified: 1
    Modified 1 file:src/utils/Card.swift

--- src/utils/Card.swift
+++ src/utils/Card.swift
@@ -10,9 +10,9 @@ class Card:Element{
         super.resolveSkin()
         Swift.print("getWidth(): " + "\(getWidth())")
         Swift.print("getHeight(): " + "\(getHeight())")
-        addSubView(Element(NaN, NaN, self, "ruler"))
+        //addSubView(Element(NaN, NaN, self, "ruler"))
         Swift.print("card.ruler created")
-        addSubView(Text(NaN, NaN, text, self, "cardText"));
+        //addSubView(Text(NaN, NaN, text, self, "cardText"));
         Swift.print("card text created")
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
