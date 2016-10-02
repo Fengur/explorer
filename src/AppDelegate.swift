852c3aaa348467cdc71a9f4231e63b18e850ac2f
Eonist <unicornhive@gmail.com>
2016-10-02 18:44:03 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -146,13 +146,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             Swift.print("is not equal")
         }
         Swift.print("newInstance!.value: " + "\(newInstance!.value)")
+        let colorValue:NSColor? = newInstance!.value as? NSColor
+        Swift.print("colorValue is NSColor: " + "\(colorValue)")
+        
+        
         let c:Any?  = NSColor.redColor()
         Swift.print("c: " + "\(c)")
         Swift.print("c is NSColor: " + "\(c is NSColor)")
         let cCopy:NSColor = c as! NSColor
         Swift.print("cCopy: " + "\(cCopy)")
-        let colorValue:NSColor? = newInstance!.value as? NSColor
-        Swift.print("colorValue is NSColor: " + "\(colorValue)")
+        
+        
         
     }
     /**
