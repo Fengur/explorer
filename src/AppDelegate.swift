301c766bd56badbec9c3616879a0eda86a951e8b
Eonist <unicornhive@gmail.com>
2016-10-05 17:55:53 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -37,7 +37,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //let color = NSColor.redColor()
         //let rgba = RGBAParser.rgba(color)
         //rgba
-        //let hex:String = RGBAParser.hex(color)
+        let hex:String = RGBAParser.hex(color)
         //HexParser.hexValue(255,255,255,100)
        // Swift.print("hex: " + "\(hex)")
         Swift.print(HexParser.hexString(255, 255, 0, 0))//FFFF0000
