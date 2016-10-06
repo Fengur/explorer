2ee3751ac4c622594edf16924e2c41d6c660abbd
Eonist <unicornhive@gmail.com>
2016-10-06 23:21:32 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -13,11 +13,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         /**/
         //let startTime = NSDate()
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css")
         //Swift.print("Adding basic styles time: " + "\(abs(startTime.timeIntervalSinceNow))")
         
         //readXMLFromDisk()
-        //win = TranslucencyWin()//Win(400,300/**//*1000,800*/)//()//
+        win = TranslucencyWin()//Win(400,300/**//*1000,800*/)//()//
         /**/
         
         //Continue here:  (Adding styles is now really fast: 0.13 sec, when loading from xml, it can be futher optimized by removing xml nodes of value that are nil)
@@ -43,10 +43,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         
         //let startTime2 = NSDate()
-        //writeXMLToDisk()
+        writeXMLToDisk()
         //Swift.print("Storing styles time: " + "\(abs(startTime2.timeIntervalSinceNow))")//0.29sec for basic styles, pretty good!
         /**/
-        rgbaColorTests()
+        //rgbaColorTests()
     }
     /**
      *
