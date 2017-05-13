2d9d3f568515da140c25cefd5529c50f55e2ab61
Eonist <30n1st@gmail.com>
2017-05-13 13:10:30 +0200

    Files modified: 1
    Modified 1 file:ExplorerApp/AppDelegate.swift

--- ExplorerApp/AppDelegate.swift
+++ ExplorerApp/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate:NSObject,NSApplicationDelegate {
     var win:NSWindow?/*<--The window must be a class variable, local variables doesn't work*/
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        //Insert code here to initialize your application
         //print(ArrayParser.difference([1,2], [1]))
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
         
