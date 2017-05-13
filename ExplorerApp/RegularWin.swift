0aba6278062d20918f155073ba3fbf49e2087238
Eonist <30n1st@gmail.com>
2017-05-13 14:08:12 +0200

    Files modified: 1
    Modified 1 file:ExplorerApp/RegularWin.swift

--- ExplorerApp/RegularWin.swift
+++ ExplorerApp/RegularWin.swift
@@ -2,24 +2,6 @@ import Cocoa
 @testable import Element
 @testable import Utils
 
-enum ViewType{
-    case basic,other,advance
-}
-extension ViewType{
-    /**
-     * Returns the mainview of the window
-     */
-    static func view(_ type:ViewType, _ size:CGSize)->NSView{
-        switch type{
-            case .basic:
-                return BasicView(size.width,size.height)
-            case .other:
-                return OtherView(size.width,size.height)
-            case .advance:
-                return AdvanceView(size.width,size.height)
-        }
-    }
-}
 class RegularWin:Window{
     init() {
         super.init(1000, 800)
@@ -35,4 +17,23 @@ class RegularWin:Window{
         (self.contentView as! Element).setSize(self.frame.size.width,self.frame.size.height)
     }
     required init(_ width:CGFloat, _ height:CGFloat) {fatalError("init has not been implemented")}
+}
+
+enum ViewType{
+    case basic,other,advance
+}
+extension ViewType{
+    /**
+     * Returns the mainview of the window
+     */
+    static func view(_ type:ViewType, _ size:CGSize)->NSView{
+        switch type{
+        case .basic:
+            return BasicView(size.width,size.height)
+        case .other:
+            return OtherView(size.width,size.height)
+        case .advance:
+            return AdvanceView(size.width,size.height)
+        }
+    }
 }
