e403740512a1a94b6c5c4efb114681962267a7de
Eonist <30n1st@gmail.com>
2017-05-14 14:39:05 +0200

    Files modified: 1
    Modified 1 file:ExplorerApp/utils/TranslucencyWin.swift

--- ExplorerApp/utils/TranslucencyWin.swift
+++ ExplorerApp/utils/TranslucencyWin.swift
@@ -1,7 +1,9 @@
 import Cocoa
 @testable import Element
 @testable import Utils
-
+/**
+ * Used for Translucent look
+ */
 class TranslucencyWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
     let w:CGFloat = 1000//500//350//
     let h:CGFloat = 800//400//300//
