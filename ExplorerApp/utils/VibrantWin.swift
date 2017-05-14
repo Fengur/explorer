226d8d183fc14d0c9ade2dc5a1ff08016cae2945
Eonist <30n1st@gmail.com>
2017-05-14 14:46:22 +0200

    Files modified: 1
    Modified 1 file:ExplorerApp/utils/VibrantWin.swift

--- ExplorerApp/utils/VibrantWin.swift
+++ ExplorerApp/utils/VibrantWin.swift
@@ -5,11 +5,13 @@ import Cocoa
  * Used for Translucent look
  */
 class VibrantWin:TranslucentWin{
-    let w:CGFloat = 1000//500//350//
-    let h:CGFloat = 800//400//300//
-    init(){
-        super.init()
-        let view = ViewType.view(.basic,CGSize(w,h))/*Sets the mainview of the window*/
-        self.contentView?.addSubview(view)
-    }
+    //let w:CGFloat = 1000//500//350//
+    //let h:CGFloat = 800//400//300//
+    /*init(contentRect: NSRect, styleMask style: NSWindowStyleMask, backing bufferingType: NSBackingStoreType, defer flag: Bool) {
+     
+     }
+     super.init()
+     let view = ViewType.view(.basic,CGSize(w,h))/*Sets the mainview of the window*/
+     self.contentView?.addSubview(view)
+     }*/
 }
