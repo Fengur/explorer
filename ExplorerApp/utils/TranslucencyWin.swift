3daead27e3259b82a78ba82310f93ea6f65c0277
Eonist <30n1st@gmail.com>
2017-05-14 14:36:39 +0200

    Files modified: 1, Files deleted: 1
    Deleted 1 file: ExplorerApp/utils/translucent/TranslucencyView.swiftModified 1 file:ExplorerApp/utils/TranslucencyWin.swift

--- ExplorerApp/utils/TranslucencyWin.swift
+++ ExplorerApp/utils/TranslucencyWin.swift
@@ -32,3 +32,36 @@ class TranslucencyWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         visualEffectView!.setFrameSize(self.frame.size)
     }
 }
+
+class TranslucencyView:NSVisualEffectView{
+    let cornerRadius:CGFloat = 8
+    override var isFlipped:Bool {return true}/*Organizes your view from top to bottom*/
+    override init(frame frameRect: NSRect) {
+        super.init(frame:frameRect)
+        self.material = .light/*AppearanceBased,Dark,MediumLight,PopOver,UltraDark,AppearanceBased,Titlebar,Menu*/
+        self.blendingMode = .behindWindow
+        self.state = .active
+        self.maskImage = maskImage(cornerRadius: cornerRadius)/*this line applies the mask to the view*/
+    }
+    override func setFrameSize(_ newSize: NSSize) {
+        super.setFrameSize(newSize)
+        self.maskImage = maskImage(cornerRadius: cornerRadius)/*<--we recreate and add the imageMask when the view resizes*/
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+
+extension TranslucencyView{
+    func maskImage(cornerRadius: CGFloat) -> NSImage {
+        let edgeLength = 2.0 * cornerRadius + 1.0
+        //Swift.print("edgeLength: " + "\(edgeLength)")
+        let maskImage = NSImage(size: NSSize(width: edgeLength, height: edgeLength), flipped: false) { rect in
+            let bezierPath = NSBezierPath(roundedRect: rect, xRadius: cornerRadius, yRadius: cornerRadius)
+            NSColor.black.set()
+            bezierPath.fill()
+            return true
+        }
+        maskImage.capInsets = EdgeInsets(top: cornerRadius, left: cornerRadius, bottom: cornerRadius, right: cornerRadius)
+        maskImage.resizingMode = .stretch
+        return maskImage
+    }
+}
